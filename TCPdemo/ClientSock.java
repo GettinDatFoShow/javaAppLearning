7e01d7dd75bf4af9bf8be53c0951aa0ef3a5c70f
Rob <robert.dale.morris@gmail.com>
2017-02-25 21:10:08 -0500

    add tcp socket demp with a server socket and a client socket class.

---
+++ TCPdemo/ClientSock.java
@@ -0,0 +1,55 @@
+/*
+ *This is a client socket class that connects to the server socket
+ * on a pre-specified of 4812, supplying a name of 'localhost'
+ * once the socket is accepted by the server a PrintStream object is created with
+ * the socket.getOutputStream() as a parameter, following this creation, a message is sent
+ * to the server. 
+ * Then, an InputStreamReader object is created using the socket as its parameter.
+ * After this, a BufferedReader object is created using the InputStreamReader object as the parameter.
+ * The socket is now ready listen for messages sent through the socket from the client.
+ * there is a while loop once the readline() function is called appon the bufferedReader object
+ * becuase the readline() will stop at the \n character. If it doesnt keep looping it will not be
+ * able to read out multiple lines from the server. 
+ * a while loop is created for the bufferedReader to be able to read multiple lines so that
+ * it does not terminate until a specified message is received. In this case the specified message is 
+ * a 'new line' character. 
+ */
+
+import java.io.*;
+import java.net.*;
+
+/**
+ *
+ * @author Robert Morris
+ */
+public class ClientSock {
+    
+   public static void main(String [] args) throws Exception{
+       
+       ClientSock client = new ClientSock();
+       client.run();
+       
+   } 
+    
+   public void run() throws Exception{
+       String messageIn = null;
+       Socket socket = new Socket("localhost", 4812);
+       PrintStream pStream = new PrintStream(socket.getOutputStream());
+       pStream.println("I NEED TO CONNECT NOWWWWW!!!!");
+       
+       InputStreamReader inRead = new InputStreamReader(socket.getInputStream());
+       BufferedReader bRead = new BufferedReader(inRead);
+       
+       
+       while((messageIn = bRead.readLine()) != null){
+            
+           if (messageIn.length() <= 2){
+                break;
+            }
+              System.out.println(messageIn);
+       }
+       socket.close();
+              
+   }
+    
+}
---
+++ TCPdemo/ServerSock.java
@@ -0,0 +1,62 @@
+/*
+ * This is a Server Socket listening on port 4812 
+ * Once a connection is heard from a client, a socket opened and accepted.
+ * from the socket instance that is created, there are a few properties available.
+ * The .getInetAdress() from the socket returns an InetAddress instance of the client on the
+ * other side of the socket. we can then call the .getHostAddress on the InetAddress object
+ * and obtain a string of the clients IPaddress.
+ * Then, an InputStreamReader object is created using the socket as its parameter.
+ * After this, a BufferedReader object is created using the InputStreamReader object as the parameter.
+ * The socket is now ready listen for messages sent through the socket from the client.
+ * there is a while loop once the readline() function is called appon the bufferedReader object
+ * becuase the readline() will stop at the \n character. If it doesnt keep looping it will not be
+ * able to read out multiple lines from the client.
+ */
+
+import java.io.*;
+import java.net.*;
+import java.util.Date;
+/**
+ *
+ * @author Robert Morris
+ */
+public class ServerSock {
+    
+    public static void main(String[] args) throws Exception{
+        
+        ServerSock Server = new ServerSock();
+        Server.run();
+        
+    }
+    
+    
+    public void run() throws Exception{
+        
+        String messageIn = null;
+        
+        ServerSocket serverSocket = new ServerSocket(4812); 
+        Socket socket = serverSocket.accept();
+        // Information Regarding the Connection.
+        InetAddress INA = socket.getInetAddress(); 
+        String hostIP = INA.getHostAddress();
+        Date date = new Date();
+        InputStreamReader inRead = new InputStreamReader(socket.getInputStream());
+        BufferedReader bRead = new BufferedReader(inRead);
+        
+        while (( messageIn = bRead.readLine()) != null){
+            if (messageIn.length() <= 2){
+                break;
+            }
+            System.out.println(messageIn);
+            String messageOut = "Your IP address is: "+ hostIP + "\n"
+                    + "It is now: " + date;
+            PrintStream pStream = new PrintStream(socket.getOutputStream());
+            pStream.println(messageOut);
+            pStream.println("");
+            break;
+           
+        }
+        socket.close();
+    }
+    
+}
